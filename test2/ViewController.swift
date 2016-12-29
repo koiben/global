5032bb1f82c100677dff03da69b98ae768e50b91
yusuke4319 <infinity314.yusuke@gmail.com>
2016-12-29 16:06:41 +0900

    1606

--- test2/ViewController.swift
+++ test2/ViewController.swift
@@ -6,20 +6,76 @@
 //  Copyright © 2016年 大木優介. All rights reserved.
 //
 
+
+
 import UIKit
 
-class ViewController: UIViewController {
 
+
+
+class ViewController: UIViewController {
+    
+    var coin = 100
+    var countNum = 0
+    var timerRunning = false
+    var timer = Timer()
+    var ruiseki_time = 0
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
+    let userDefaults = UserDefaults.standard
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 以前のデータがある場合のみ、coinを更新
+        if userDefaults.integer(forKey: "testInt") > 0{
+            coin = userDefaults.integer(forKey: "testInt")
+        }
+        label2.text = "コイン：\(coin)"
+        }
+    
+    func update() {
+        countNum += 1;
+        label.text = "\(countNum)"
     }
 
+    @IBAction func start(_ sender: AnyObject) {
+        
+        if timerRunning == false {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+            timerRunning = true
+        }
+    }
+    
+    
+    @IBAction func reset(_ sender: AnyObject) {
+        if timerRunning == true {
+            timer.invalidate()
+            timerRunning = false
+        }
+        ruiseki_time = ruiseki_time + countNum;
+        coin = coin + ruiseki_time/5;
+        countNum = 0
+        ruiseki_time = 0
+        label.text = "0"
+        label2.text = "コイン：\(coin)"
+        
+        //Int型のデータを保存
+        userDefaults.set(coin, forKey: "testInt")
+        
+        //plistファイルへの出力と同期する。
+        userDefaults.synchronize()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
